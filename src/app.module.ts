802ef403e7d07df5cf4a71e45e3d007ffe4d767c
rickwu7 <133179612+rickwu7@users.noreply.github.com>
2023-05-16 10:41:40 +1200

    feat: NestJS with TypeORM and MySQL

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Book } from './library/entities/book.entity';
+import { LibraryModule } from './library/library.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123',
+      database: 'test_db',
+      entities: [Book],
+      synchronize: true,
+      dropSchema: false
+    }), LibraryModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
